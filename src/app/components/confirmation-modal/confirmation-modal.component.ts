43f5116982e8fe2c71403b277b6e48e9a4c20033
Pavel Mena <pavel.mena@ntsprint.com>
2023-01-20 09:53:03 +0100

    LVL3 - Step 2

--- src/app/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/components/confirmation-modal/confirmation-modal.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-confirmation-modal',
   templateUrl: './confirmation-modal.component.html',
   styleUrls: ['./confirmation-modal.component.css']
 })
-export class ConfirmationModalComponent implements OnInit {
+export class ConfirmationModalComponent {
+  @Input() modalTitle = "Title";
+  @Input() closeText = "Close";
+  @Input() yesText = "Yes";
+  @Input() btnSubmitClass = 'btn-primary';
+  @Output() confirmationEvent = new EventEmitter<null>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  confirm() {
+    setTimeout(() => { //Allow animation to end
+      this.confirmationEvent.emit();
+    }, 300);
   }
 
 }
