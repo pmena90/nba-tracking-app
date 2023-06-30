fe1569fa698af47f81951a1e017e9437233fb180
Pavel Mena <pavel.mena1990@gmail.com>
2023-06-30 11:40:48 +0200

    Adding coverage xml report

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'cobertura', file: 'coverage.xml' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
